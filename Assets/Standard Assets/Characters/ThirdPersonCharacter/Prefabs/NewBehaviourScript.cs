4f15a165e0831cdbb70708ac5aaab674fbe463ec
zhuoIC <nicolimonar@gmail.com>
2017-09-29 10:30:56 +0200

    Añadiendo los demas archivos

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Prefabs/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+	float contadorX = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (transform.rotation.x + contadorX, transform.rotation.y, transform.rotation.z));
+	}
+}
